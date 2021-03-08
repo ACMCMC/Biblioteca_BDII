6f428c08afde2db8037bd992575e0247d5ab929f
ACMCMC <20495460+ACMCMC@users.noreply.github.com>
2021-03-08 20:22:23 +0100

    Prestamo

--- src/baseDatos/DAOPrestamos.java
+++ src/baseDatos/DAOPrestamos.java
@@ -30,7 +30,7 @@ public class DAOPrestamos extends AbstractDAO {
 
         try {
             stat = con.prepareStatement(
-                    "select prestamo.fecha_prestamo, prestamo.fecha_devolucion, public.get_fecha_vencimiento(prestamo.ejemplar, prestamo.fecha_prestamo, prestamo.usuario, prestamo.libro), prestamo.ejemplar, prestamo.libro FROM prestamo WHERE prestamo.usuario=?");
+                    "select prestamo.fecha_prestamo, prestamo.fecha_devolucion, public.get_fecha_vencimiento(prestamo.fecha_prestamo), prestamo.ejemplar, prestamo.libro FROM prestamo WHERE prestamo.usuario=?");
                     stat.setString(1, u.getIdUsuario());
             ResultSet res = stat.executeQuery();
             while (res.next()) {
@@ -47,6 +47,31 @@ public class DAOPrestamos extends AbstractDAO {
             }
         }
         return resultado;
+    }
+
+    public void insertarPrestamo(Prestamo p) {
+        Connection con = getConexion();
+        PreparedStatement stat = null;
+
+        try {
+            stat = con.prepareStatement(
+                    "insert into prestamo(ejemplar, fecha_prestamo, libro, usuario, fecha_devolucion) values (?,?,?,?,?)");
+                    stat.setInt(1, p.getEjemplar().getNumEjemplar());
+                    stat.setDate(2, p.getFechaPrestamo());
+                    stat.setInt(3, p.getEjemplar().getLibro().getIdLibro());
+                    stat.setString(4, p.getUsuario().getIdUsuario());
+                    stat.setDate(4, p.getFechaDevolucion());
+            stat.executeQuery();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            this.getFachadaAplicacion().muestraExcepcion(e.getMessage());
+        } finally {
+            try {
+                stat.close();
+            } catch (SQLException e) {
+                System.out.println("Imposible cerrar cursores");
+            }
+        }
     }
 
 }
